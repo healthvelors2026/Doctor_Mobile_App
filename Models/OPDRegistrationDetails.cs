44ea5c547e2fdc2c1ab973887b14e0f258013400
chetan <chintuMkwana10@gmail.com>
2026-08-14 15:02:03 +0530

    update code

--- Models/OPDRegistrationDetails.cs
+++ Models/OPDRegistrationDetails.cs
@@ -20,6 +20,7 @@
         public bool ClassForReimbursement { get; set; }
         public int RateBasedOn { get; set; }
         public int ChargeType { get; set; }
+        public string? CRNumber { get; set; }
         public string? PatientName { get; set; }
         public string? DoctorName { get; set; }
         public string? Gender { get; set; }
